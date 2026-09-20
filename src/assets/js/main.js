80775fc28df319a393fc9066460d54de8bb353ed
Aman <forcodingakp@gmail.com>
2026-09-20 11:06:15 +0530

    Downfall videos: autoplay embedded, full width, drop click-to-play

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,6 @@
 // Entry point. Each module is independent and safe to run on any page.
 import { observeFigures } from "./modules/figure-observer.js";
 import { initSectionNav } from "./modules/section-nav.js";
-import { initVideoEmbeds } from "./modules/video-embed.js";
 import { initGrainFields } from "./modules/grain-fields.js";
 import { initIsroTimeline } from "./modules/figures/isro-timeline.js";
 import { initMedmnistRouter } from "./modules/figures/medmnist-router.js";
@@ -10,7 +9,6 @@ import { initDownfallSim } from "./modules/figures/downfall-sim.js";
 
 observeFigures();
 initSectionNav();
-initVideoEmbeds();
 initGrainFields();
 initIsroTimeline();
 initMedmnistRouter();
